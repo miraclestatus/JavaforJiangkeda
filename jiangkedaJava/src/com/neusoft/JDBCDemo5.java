da172d01a15414413448e9e09429506d74d23056
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-11-19 10:37:19 +0800

    1111

---
+++ jiangkedaJava/src/com/neusoft/JDBCDemo5.java
@@ -0,0 +1,51 @@
+package com.neusoft;
+
+import com.neusoft.domain.Emp;
+
+import java.sql.*;
+
+/**
+ * 查询emp表中所有数据
+ */
+public class JDBCDemo5 {
+    public static void main(String[] args) throws Exception {
+        //        1、导入驱动jar包, 建立libs文件夹， 右键 add as library
+        //        2、 注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //        3、获取数据库连接对象
+        Connection conn = DriverManager.getConnection("jdbc:mysql://" +
+                "localhost:3306/jiangkeda","root", "root");
+        //        4、定义sql
+        String sql = "select * from emp";
+        //        5、获取数据库连接对象statement
+        Statement stmt = conn.createStatement();
+        //        6、执行sql
+        ResultSet rs = stmt.executeQuery(sql);
+        //        7、处理结果
+//        while (rs.next()){
+//            int empno = rs.getInt(1);
+//            String eName = rs.getString(2);
+//            String job = rs.getString(3);
+//            int mgr = rs.getInt(4);
+//            Date hiredata = rs.getDate(5);
+//            int sal = rs.getInt(6);
+//            int comm = rs.getInt(7);
+//            int deptno = rs.getInt(8);
+//            System.out.println(empno+"----"+eName+"----"+job);
+//        }
+        rs.next();
+        int empno = rs.getInt(1);
+        String eName = rs.getString(2);
+        String job = rs.getString(3);
+        int mgr = rs.getInt(4);
+        Date hiredata = rs.getDate(5);
+        int sal = rs.getInt(6);
+        int comm = rs.getInt(7);
+        int deptno = rs.getInt(8);
+        Emp emp = new Emp(empno, eName, job, mgr, hiredata, sal, comm, deptno);
+        System.out.println(emp);
+        //        8、释放资源
+        stmt.close();
+        conn.close();
+    }
+}
---
+++ jiangkedaJava/src/com/neusoft/domain/Emp.java
@@ -0,0 +1,110 @@
+package com.neusoft.domain;
+
+import java.util.Date;
+
+/**
+ * 与数据库中表字段一一对应的 实体类
+ * 定义形式为JavaBean
+ */
+public class Emp {
+    private int empno;
+    private String ename;
+    private String job;
+    private int mgr;
+    private Date hireDate;
+    private int sal;
+    private int comm;
+    private int deptno;
+
+    public Emp() {
+    }
+
+    public Emp(int empno, String ename, String job, int mgr, Date hireDate, int sal, int comm, int deptno) {
+        this.empno = empno;
+        this.ename = ename;
+        this.job = job;
+        this.mgr = mgr;
+        this.hireDate = hireDate;
+        this.sal = sal;
+        this.comm = comm;
+        this.deptno = deptno;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(int mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public int getSal() {
+        return sal;
+    }
+
+    public void setSal(int sal) {
+        this.sal = sal;
+    }
+
+    public int getComm() {
+        return comm;
+    }
+
+    public void setComm(int comm) {
+        this.comm = comm;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "empno=" + empno +
+                ", ename='" + ename + '\'' +
+                ", job='" + job + '\'' +
+                ", mgr=" + mgr +
+                ", hireDate=" + hireDate +
+                ", sal=" + sal +
+                ", comm=" + comm +
+                ", deptno=" + deptno +
+                '}';
+    }
+}
