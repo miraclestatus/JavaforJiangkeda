261887f55d03e5efee32d9021106516fdca1daf2
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-11-19 09:12:49 +0800

    1121

--- jiangkedaJava/src/com/neusoft/JDBCDemo3.java
+++ jiangkedaJava/src/com/neusoft/JDBCDemo3.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 /**
- * 插入操作
+ * 删除操作
  */
-public class JDBCDemo2 {
+public class JDBCDemo3 {
     public static void main(String[] args) throws Exception {
         //        1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         //        2、 注册驱动
@@ -16,7 +16,7 @@ public class JDBCDemo2 {
         Connection conn = DriverManager.getConnection("jdbc:mysql://" +
                 "localhost:3306/jiangkeda","root", "root");
         //        4、定义sql
-        String sql = "insert into account values (null , '赵六', 888)";
+        String sql = "delete from account where id = 4";
         //        5、获取数据库连接对象statement
         Statement stmt = conn.createStatement();
         //        6、执行sql
