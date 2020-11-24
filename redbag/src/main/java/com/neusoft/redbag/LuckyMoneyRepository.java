e96cc90700c12e2a2b90354206afb46fcf4932ac
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-11-24 17:13:17 +0800

    1111

---
+++ redbag/src/main/java/com/neusoft/redbag/LuckyMoneyRepository.java
@@ -0,0 +1,6 @@
+package com.neusoft.redbag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LuckyMoneyRepository extends JpaRepository<LuckyMoney, Integer> {
+}
---
+++ redbag/src/test/java/com/neusoft/redbag/LuckyMoneyRepositoryTests.java
@@ -0,0 +1,25 @@
+package com.neusoft.redbag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LuckyMoneyRepositoryTests {
+
+    @Autowired
+    private LuckyMoneyRepository repository;
+
+    @Test
+    public void listAll(){
+        List<LuckyMoney> list = repository.findAll();
+        for(LuckyMoney luckyMoney: list){
+            System.out.println(luckyMoney);
+        }
+    }
+}
